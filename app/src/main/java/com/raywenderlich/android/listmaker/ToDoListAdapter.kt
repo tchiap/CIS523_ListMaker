a12f734415d6e6bb2ed690acf0eaec7d90e6c63e
tjc8402 <57610534+tjc8402@users.noreply.github.com>
2020-10-11 20:07:28 -0500

    Completed Lesson 6 - Build an Adapter

---
+++ app/src/main/java/com/raywenderlich/android/listmaker/ToDoListAdapter.kt
@@ -0,0 +1,19 @@
+package com.raywenderlich.android.listmaker
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ToDoListAdapter : RecyclerView.Adapter<ToDoListViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ToDoListViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ToDoListViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
---
+++ app/src/main/java/com/raywenderlich/android/listmaker/ToDoListViewHolder.kt
@@ -0,0 +1,7 @@
+package com.raywenderlich.android.listmaker
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class ToDoListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+}
