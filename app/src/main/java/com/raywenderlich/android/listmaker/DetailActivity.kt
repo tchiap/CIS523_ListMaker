9ef645b485a2d17003f1f23c2822bb2d5e230b84
tjc8402 <57610534+tjc8402@users.noreply.github.com>
2020-10-12 16:26:57 -0500

    Completed Lesson 26 - Challenge:  Set up a RecyclerView

--- app/src/main/java/com/raywenderlich/android/listmaker/DetailActivity.kt
+++ app/src/main/java/com/raywenderlich/android/listmaker/DetailActivity.kt
@@ -2,18 +2,25 @@ package com.raywenderlich.android.listmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class DetailActivity : AppCompatActivity() {
 
     lateinit var list: TaskList
+    lateinit var taskListRecyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Lesson 25 - Bringing Everything Together
+        // Lesson 25 - Bring Everything Together
         setContentView(R.layout.activity_detail)
 
         list = intent.getParcelableExtra(MainActivity.INTENT_LIST_KEY) as TaskList
         title = list.name
+
+        // Lesson 26
+        taskListRecyclerView = findViewById(R.id.task_list_recyclerview)
+        taskListRecyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
