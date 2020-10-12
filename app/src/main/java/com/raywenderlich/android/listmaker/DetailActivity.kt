c903e34866874c6ddc22956e8cd3c22408d24edd
tjc8402 <57610534+tjc8402@users.noreply.github.com>
2020-10-12 18:55:42 -0500

    Completed Lesson 28 - Add Task List Items

--- app/src/main/java/com/raywenderlich/android/listmaker/DetailActivity.kt
+++ app/src/main/java/com/raywenderlich/android/listmaker/DetailActivity.kt
@@ -2,13 +2,19 @@ package com.raywenderlich.android.listmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class DetailActivity : AppCompatActivity() {
 
     lateinit var list: TaskList
     lateinit var taskListRecyclerView: RecyclerView
+
+    lateinit var addTaskButton : FloatingActionButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,5 +31,31 @@ class DetailActivity : AppCompatActivity() {
 
         // Lesson 27
         taskListRecyclerView.adapter = TaskListAdapter(list)
+
+        // Lesson 28
+        addTaskButton = findViewById(R.id.add_task_button)
+        addTaskButton.setOnClickListener {
+            showCreateTaskDialog()
+        }
+    }
+
+    private fun showCreateTaskDialog() {
+
+        // Lesson 28
+        val taskEditText = EditText(this)
+        taskEditText.inputType = InputType.TYPE_CLASS_TEXT
+
+        AlertDialog.Builder(this)
+            .setTitle(R.string.task_to_add)
+            .setView(taskEditText)
+            .setPositiveButton(R.string.add_task) {
+                dialog, _ ->
+                    val task = taskEditText.text.toString()
+                    list.tasks.add(task)
+                    dialog.dismiss()
+            }
+            .create()
+            .show()
+
     }
 }
